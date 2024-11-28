4450182e16c5ffb8027990a774db74c171bfae7a
Gagik <gagik.n@gmail.com>
2024-11-28 14:37:04 +0100

    ✨ feat(setup): initialize next-handlebars package
    - add initial index.js with a console log function
    - create package.json with basic metadata and script
    
    🔧 chore(config): add config files and dependencies
    
    - add .gitignore and .npmignore for ignoring files in git and npm
    - create .vscode settings for word suggestions
    - initialize tsconfig.json with TypeScript configuration
    - setup tsup.config.ts for building the project
    - include package-lock.json with dependencies details
    
    ♻️ refactor(structure): organize initial file structure
    
    - add placeholder function in place-holder-function.ts
    - export from index.ts for module setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# next-handlebars
+This an attempt to create a Nextjs project with handlebars as the templating engine.
---
+++ src/place-holder-function.ts
@@ -0,0 +1,6 @@
+const placeholder = () => {
+  console.log('This will be handlebars plugin for next js route')
+  return 'This will be handlebars plugin for next js route'
+}
+
+export default placeholder
